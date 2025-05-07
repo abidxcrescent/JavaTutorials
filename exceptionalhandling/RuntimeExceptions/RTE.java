60ff1b61a67bcc55e427f44ebd8cd0f4c2c2a0f1
Abid Ali <mohdabidaliusa@gmail.com>
2025-05-07 15:59:31 -0700

    Runtime Exceptions

---
+++ exceptionalhandling/RuntimeExceptions/RTE.java
@@ -0,0 +1,34 @@
+package exceptionalhandling.RuntimeExceptions;
+
+public class RTE {
+
+    public static void main(String[] args) {
+
+        // Class Cast Exception
+        /*
+        Object num = 0;
+        System.out.println((String) num);
+        */
+
+        // Arithmetic Exception
+//        System.out.println(5/0);
+
+//        // IndexOutOfBoundException
+//        int [] nums = new int[5];
+//        System.out.println(nums[6]);
+
+//        String name  = "abid";
+//        System.out.println(name.charAt(6));
+
+//        // Null Pointer Exception
+//        String name = null;
+//        System.out.println(name.charAt(0));
+
+//        // Illegal Argument Exception
+//        int val = Integer.parseInt("abc");
+//        System.out.println(val);
+
+
+    }
+
+}
